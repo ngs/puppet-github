5ff32f7dd4e896609150122a6331a0550aa4d1a3
Paul Chechetin <paulche@yandex.ru>
2014-02-15 10:24:38 +0400

    Update version to latest

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@
 # include github_for_mac
 class github_for_mac {
   package { 'GitHubForMac':
-    source   => 'https://github-central.s3.amazonaws.com/mac/812e9f1a-81f9-11e2-842a-d627744bf6bd.zip',
+    source   => 'https://github-central.s3.amazonaws.com/mac/GitHub%20for%20Mac%20171.zip',
     provider => 'compressed_app'
   }
 }
